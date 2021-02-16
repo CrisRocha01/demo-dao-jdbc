22bb923f854f2c2a445b4fa1faecbc4b7df38a98
CrisRocha01 <cristiano_g_rocha@yahoo.com.br>
2021-02-15 21:58:30 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entity.Department;
+import model.entity.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
 		Department test = new Department(1, "test");
 		
-		System.out.println("test");
+		Seller seller = new Seller(21, "bob", "bob@gmai.com", new Date(), 3000.00, test);
+		
+		System.out.println(test);
+		
+		System.out.println(seller);
 	}
 
 }
