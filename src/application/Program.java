861c972da539ae2164da1e7bb6ba430942571e39
CrisRocha01 <cristiano_g_rocha@yahoo.com.br>
2021-02-15 21:45:25 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entity.Department;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Department test = new Department(1, "test");
+		
+		System.out.println("test");
+	}
+
+}
